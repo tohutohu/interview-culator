9af70a7bdf0f4ba6dfda16763290f1067878b7f0
to-hutohu <tohu.soy@gmail.com>
2016-07-28 17:11:39 +0900

    mysqlの追加

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var mysql = require("mysql2");
+var connection = mysql.createConnection({
+    host : 'ja-cdbr-azure-west-a.cloudapp.net',
+    user : 'b5df9e16ecc0d7',
+    password : '6e9f49a2',
+    port : 3306 ,
+    database : 'feedtank'
+  });
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });

51afbc6c25f678ceaf8517f5012f6315123dad5e
to-hutohu <tohu.soy@gmail.com>
2016-08-02 01:17:31 +0900

    are

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require("mysql2");
-var connection = mysql.createConnection({
-    host : 'ja-cdbr-azure-west-a.cloudapp.net',
-    user : 'b5df9e16ecc0d7',
-    password : '6e9f49a2',
-    port : 3306 ,
-    database : 'feedtank'
-  });
+var connection = require("./myconnection");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var query = 'SELECT * FROM feed';
+  var query = 'SELECT * ,DATE_FORMAT(pubdate, \'%Y年%m月%d日 %k時%i分\') AS pubdate FROM feed';
   connection.query(query,function(err,rows){
     res.render('index', { title: 'Express',
                           items: rows});

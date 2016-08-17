9de0d864ffce78e82e05bd9b87ad87392dd5a010
とふとふ <tohu.soy@gmail.com>
2016-08-18 00:19:58 +0900

    ページ送り機能の実装

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var connection = require("./myconnection");
+var url = require("url");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var query = 'SELECT * ,DATE_FORMAT(pubdate, \'%Y年%m月%d日 %k時%i分\') AS pubdate FROM feed';
+  var page = req.query.p;
+  if(page==null || page=="0"){
+    page=1;
+  }
+  page = parseInt(page);
+  console.log(page);
+  var query = 'SELECT * ,DATE_FORMAT(pubdate, \'%Y年%m月%d日 %k時%i分\') AS pubdate FROM feed LIMIT '+(page-1)*15 +', 15';
   connection.query(query,function(err,rows){
-    res.render('index.html', { title: 'Express',
-                          items: rows});
+    res.render('index.html', { title: 'Express',items: rows,page: page});
   });
 });
 

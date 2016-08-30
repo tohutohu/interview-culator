310cde5ef049989e082a6d501bb7149737edf812
とふとふ <tohu.soy@gmail.com>
2016-08-31 04:26:27 +0900

    no message

--- routes/item.js
+++ routes/item.js
@@ -6,11 +6,13 @@ var connection=require('./myconnection');
 router.get('/:id', function(req, res, next) {
   var rank_query = 'SELECT * FROM feed ORDER BY pv DESC LIMIT 5; ';
   var query = rank_query + 'SELECT * FROM feed WHERE id = ' + req.params.id;
+  connection.connect();
   connection.query(query,function(err,itemdetail){
     console.log(itemdetail[1].pv);
     res.render('item.html',{title:'インタビューキュレーター',item:itemdetail[1],rank:itemdetail[0]});
   });
   connection.query('UPDATE feed SET pv = pv + 1 WHERE id = '+req.params.id+";");
+  connection.end();
 });
 
 module.exports = router;

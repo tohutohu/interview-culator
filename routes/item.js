e8376d8c524ee8d4fc5ac953ad6a9e728db8d7cd
とふとふ <tohu.soy@gmail.com>
2016-09-06 12:07:30 +0900

    no message

--- routes/item.js
+++ routes/item.js
@@ -5,12 +5,12 @@ var connection=require('./myconnection');
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
   var rank_query = 'SELECT * FROM feed ORDER BY pv DESC LIMIT 5; ';
-  var query = rank_query + 'SELECT * FROM feed WHERE id = ' + req.params.id + ';';
+  var query = rank_query + 'SELECT * FROM feed WHERE id = ' + req.params.id + ';SELECT * FROM item WHERE id ='+ req.params.id + ";";
   connection.getConnection(function(er,con){
     con.query(query,function(err,itemdetail){
       if(err) console.log(err);
-      console.log(itemdetail[1].pv);
-      res.render('item.html',{title:'インタビューキュレーター|'+itemdetail[1].title,item:itemdetail[1],rank:itemdetail[0]});
+      console.log(itemdetail[2][0].tags.split(/\n/));
+      res.render('item.html',{title:itemdetail[1][0].title+'｜インタビューキュレーター',i:itemdetail[1][0],rank:itemdetail[0],tags:itemdetail[2][0].tags.split(/\n/)});
     });
     con.query('UPDATE feed SET pv = pv + 1 WHERE id = '+req.params.id+";");
     con.release();
